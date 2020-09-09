6ffcd7daab5d82994e8999f8c4a5a39552a230f3
Chong Kim <chong@undercovertourist.com>
2020-09-09 10:02:18 -0500

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import os
 from setuptools import setup, find_packages
-version = '0.3.3.3'
+version = '0.3.3.4'
 README = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(README).read()
 setup(name='django-locking',
